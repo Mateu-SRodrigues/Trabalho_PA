663e7aa776678557dfbe4e5b050652f5428af67c
Lz-Miguel <98miguel17@gmail.com>
2026-06-07 16:23:47 -0300

    Nesses arquivos estão a biblioteca de emprestimos.h onde tem as funções para gerenciar tudo relacionado aos emprestimos(Seja matricula do aluno, datas tanto de quando alguem aluga ou quando alguem atrasa a devolução de algum livro e tambem o codigo do(s) livro(s) que foram alugados).

---
+++ emprestimos.h
@@ -0,0 +1,28 @@
+#ifndef EMPRESTIMOS_H
+#define EMPRESTIMOS_H
+
+typedef struct {
+
+    int id;                     
+    int matricula_usuario;      
+    int codigo_livro;           
+    char data_retirada[11];     
+    char data_prevista[11];     
+    char data_devolucao[11];    
+    int devolvido;              
+                                
+
+} Emprestimo;
+
+extern Emprestimo emprestimos[1000];
+extern int totalEmprestimos;
+
+void realizarEmprestimo();
+void registrarDevolucao();
+void listarEmprestimos();
+void listarEmprestimosAtrasados();
+void gerarDataAtual(char data[]);
+void gerarDataPrevista(char data[]);
+int verificarAtraso(char dataPrevista[]);
+
+#endif
