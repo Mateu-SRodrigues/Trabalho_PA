b3e4a9441631597e7a9e1f813a5658871bee5dac
Nicollaslino <103421158+Nicollaslino@users.noreply.github.com>
2026-06-15 12:39:31 -0300

    contém os protótipos das funções responsáveis pela geração dos relatórios do sistema de biblioteca

---
+++ relatorios.h
@@ -0,0 +1,23 @@
+#ifndef RELATORIOS_H
+#define RELATORIOS_H
+
+void relatorio_AcervoDisponivel(
+    Livro livros[],
+    int qtdLivros);
+
+void relatorio_Livros_Mais_Emprestados(
+    Livro livros[],
+    int qtdLivros);
+
+void relatorio_Atrasados(
+    Usuario usuarios[],
+    int qtdUsuarios,
+    Emprestimo emprestimos[],
+    int qtdEmprestimos);
+
+void historico_Usuario(
+    int matricula,
+    Emprestimo emprestimos[],
+    int qtdEmprestimos);
+
+#endif
