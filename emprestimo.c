5bd9c4b179dcd3fc4ce033a053090eb52e18a417
Lz-Miguel <98miguel17@gmail.com>
2026-06-15 15:28:06 -0300

    Parte dos emprestimos
    Nesses arquivos estão a biblioteca de emprestimos.c onde tem as funções para gerenciar tudo relacionado aos emprestimos(Seja matricula do aluno, datas tanto de quando alguem aluga ou quando alguem atrasa a devolução de algum livro e tambem o codigo do(s) livro(s) que foram alugados).

--- emprestimo.c
+++ emprestimo.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <string.h>
-#include <time.h> 
+#include <time.h>
 
 #include "emprestimos.h"
+#include "livros.h"
 
 // Vetor de empréstimos.
 
 Emprestimo emprestimos[1000];
 
-// Quantidade de empréstimos cadastrados.
+// Quantidade atual de empréstimos.
 
 int totalEmprestimos = 0;
 
@@ -20,14 +21,16 @@ void gerarDataAtual(char data[]) {
 
     struct tm tm = *localtime(&t);
 
-    sprintf(data,
-            "%02d/%02d/%d",
-            tm.tm_mday,
-            tm.tm_mon + 1,
-            tm.tm_year + 1900);
+    sprintf(
+        data,
+        "%02d/%02d/%d",
+        tm.tm_mday,
+        tm.tm_mon + 1,
+        tm.tm_year + 1900
+    );
 }
 
-// Gera a data prevista para devolução. (14 dias após a retirada)
+// Gera data prevista (+14 dias).
 
 void gerarDataPrevista(char data[]) {
 
@@ -37,14 +40,16 @@ void gerarDataPrevista(char data[]) {
 
     struct tm tm = *localtime(&t);
 
-    sprintf(data,
-            "%02d/%02d/%d",
-            tm.tm_mday,
-            tm.tm_mon + 1,
-            tm.tm_year + 1900);
+    sprintf(
+        data,
+        "%02d/%02d/%d",
+        tm.tm_mday,
+        tm.tm_mon + 1,
+        tm.tm_year + 1900
+    );
 }
 
-// Cria um novo empréstimo.
+// Realiza um empréstimo.
 
 void realizarEmprestimo() {
 
@@ -58,15 +63,52 @@ void realizarEmprestimo() {
     printf("Codigo do livro: ");
     scanf("%d", &e.codigo_livro);
 
-    // Depois será integrado com os módulos de livros e usuários.
+    // Procura o livro.
+    
+    int posLivro =
+        buscarIndiceLivroPorCodigo(
+            e.codigo_livro
+        );
+
+    if(posLivro == -1) {
+
+        printf("Livro nao encontrado!\n");
+
+        return;
+    }
+
+    // Verifica disponibilidade.
+    
+    if(livros[posLivro].qtd_disponivel <= 0) {
+
+        printf(
+            "Nao existem exemplares disponiveis!\n"
+        );
+
+        return;
+    }
+
+    /*
+        Quando usuarios.c ficar pronto:
+
+        - verificar usuario
+        - verificar limite de 3 livros
+    */
 
     e.id = totalEmprestimos + 1;
 
-    gerarDataAtual(e.data_retirada);
+    gerarDataAtual(
+        e.data_retirada
+    );
 
-    gerarDataPrevista(e.data_prevista);
+    gerarDataPrevista(
+        e.data_prevista
+    );
 
-    strcpy(e.data_devolucao, "");
+    strcpy(
+        e.data_devolucao,
+        ""
+    );
 
     e.devolvido = 0;
 
@@ -74,10 +116,32 @@ void realizarEmprestimo() {
 
     totalEmprestimos++;
 
-    printf("\nEmprestimo realizado com sucesso!\n");
+    /*
+        Atualiza estoque
+    */
+    livros[posLivro]
+        .qtd_disponivel--;
+
+    livros[posLivro]
+        .total_emprestimos++;
+
+    printf(
+        "\nEmprestimo realizado com sucesso!\n"
+    );
+
+    printf(
+        "Data retirada: %s\n",
+        e.data_retirada
+    );
+
+    printf(
+        "Data prevista: %s\n",
+        e.data_prevista
+    );
 }
 
-// Registra a devolução de um livro.
+
+// Registra devolução.
 
 void registrarDevolucao() {
 
@@ -86,91 +150,154 @@ void registrarDevolucao() {
     printf("\n===== DEVOLUCAO =====\n");
 
     printf("ID do emprestimo: ");
+
     scanf("%d", &id);
 
-    for(int i = 0; i < totalEmprestimos; i++) {
+    for(
+        int i = 0;
+        i < totalEmprestimos;
+        i++
+    ) {
 
-        if(emprestimos[i].id == id) {
+        if(
+            emprestimos[i].id == id
+        ) {
 
-            if(emprestimos[i].devolvido == 1) {
+            if(
+                emprestimos[i]
+                .devolvido == 1
+            ) {
 
-                printf("Livro ja devolvido!\n");
+                printf(
+                    "Livro ja devolvido!\n"
+                );
 
                 return;
             }
 
+            int posLivro =
+                buscarIndiceLivroPorCodigo(
+                    emprestimos[i]
+                    .codigo_livro
+                );
+
+            if(posLivro != -1) {
+
+                livros[posLivro]
+                    .qtd_disponivel++;
+            }
+
             gerarDataAtual(
-                emprestimos[i].data_devolucao
+                emprestimos[i]
+                .data_devolucao
             );
 
-            emprestimos[i].devolvido = 1;
+            emprestimos[i]
+                .devolvido = 1;
 
-            printf("Devolucao registrada!\n");
+            printf(
+                "Devolucao registrada com sucesso!\n"
+            );
 
             return;
         }
     }
 
-    printf("Emprestimo nao encontrado!\n");
+    printf(
+        "Emprestimo nao encontrado!\n"
+    );
 }
 
-// Mostra todos os empréstimos.
+// Lista todos os empréstimos.
 
 void listarEmprestimos() {
 
-    printf("\n===== EMPRESTIMOS =====\n");
-
     if(totalEmprestimos == 0) {
 
-        printf("Nenhum emprestimo cadastrado.\n");
+        printf(
+            "Nenhum emprestimo cadastrado!\n"
+        );
 
         return;
     }
 
-    for(int i = 0; i < totalEmprestimos; i++) {
+    for(
+        int i = 0;
+        i < totalEmprestimos;
+        i++
+    ) {
 
-        printf("\nID: %d\n",
-               emprestimos[i].id);
+        printf(
+            "\nID: %d\n",
+            emprestimos[i].id
+        );
 
-        printf("Matricula: %d\n",
-               emprestimos[i].matricula_usuario);
+        printf(
+            "Matricula: %d\n",
+            emprestimos[i]
+            .matricula_usuario
+        );
 
-        printf("Codigo Livro: %d\n",
-               emprestimos[i].codigo_livro);
+        printf(
+            "Livro: %d\n",
+            emprestimos[i]
+            .codigo_livro
+        );
 
-        printf("Retirada: %s\n",
-               emprestimos[i].data_retirada);
+        printf(
+            "Retirada: %s\n",
+            emprestimos[i]
+            .data_retirada
+        );
 
-        printf("Prevista: %s\n",
-               emprestimos[i].data_prevista);
+        printf(
+            "Prevista: %s\n",
+            emprestimos[i]
+            .data_prevista
+        );
 
-        if(emprestimos[i].devolvido) {
+        if(
+            emprestimos[i]
+            .devolvido
+        ) {
 
-            printf("Devolucao: %s\n",
-                   emprestimos[i].data_devolucao);
+            printf(
+                "Devolucao: %s\n",
+                emprestimos[i]
+                .data_devolucao
+            );
 
-            printf("Status: DEVOLVIDO\n");
+            printf(
+                "Status: DEVOLVIDO\n"
+            );
         }
         else {
 
-            printf("Status: EM ABERTO\n");
+            printf(
+                "Status: EM ABERTO\n"
+            );
         }
     }
 }
 
-// Verifica se uma data já passou.
 
-int verificarAtraso(char dataPrevista[]) {
+// Verifica atraso.
+
+int verificarAtraso(
+    char dataPrevista[]
+) {
 
     int dia;
     int mes;
     int ano;
 
-    sscanf(dataPrevista,
-           "%d/%d/%d",
-           &dia,
-           &mes,
-           &ano);
+    sscanf(
+        dataPrevista,
+        "%d/%d/%d",
+        &dia,
+        &mes,
+        &ano
+    );
 
     struct tm data = {0};
 
@@ -178,11 +305,18 @@ int verificarAtraso(char dataPrevista[]) {
     data.tm_mon = mes - 1;
     data.tm_year = ano - 1900;
 
-    time_t prazo = mktime(&data);
+    time_t prazo =
+        mktime(&data);
 
-    time_t agora = time(NULL);
+    time_t agora =
+        time(NULL);
 
-    if(difftime(agora, prazo) > 0) {
+    if(
+        difftime(
+            agora,
+            prazo
+        ) > 0
+    ) {
 
         return 1;
     }
@@ -191,39 +325,63 @@ int verificarAtraso(char dataPrevista[]) {
 }
 
 
-// Lista apenas empréstimos atrasados.
+// Lista empréstimos atrasados.
 
 void listarEmprestimosAtrasados() {
 
     int encontrou = 0;
 
-    printf("\n===== ATRASADOS =====\n");
+    printf(
+        "\n===== EMPRESTIMOS ATRASADOS =====\n"
+    );
 
-    for(int i = 0; i < totalEmprestimos; i++) {
+    for(
+        int i = 0;
+        i < totalEmprestimos;
+        i++
+    ) {
 
-        if(emprestimos[i].devolvido == 0 &&
-           verificarAtraso(
-               emprestimos[i].data_prevista
-           )) {
+        if(
+            emprestimos[i]
+            .devolvido == 0
+            &&
+            verificarAtraso(
+                emprestimos[i]
+                .data_prevista
+            )
+        ) {
 
             encontrou = 1;
 
-            printf("\nID: %d\n",
-                   emprestimos[i].id);
+            printf(
+                "\nID: %d\n",
+                emprestimos[i].id
+            );
 
-            printf("Usuario: %d\n",
-                   emprestimos[i].matricula_usuario);
+            printf(
+                "Usuario: %d\n",
+                emprestimos[i]
+                .matricula_usuario
+            );
 
-            printf("Livro: %d\n",
-                   emprestimos[i].codigo_livro);
+            printf(
+                "Livro: %d\n",
+                emprestimos[i]
+                .codigo_livro
+            );
 
-            printf("Data Prevista: %s\n",
-                   emprestimos[i].data_prevista);
+            printf(
+                "Data prevista: %s\n",
+                emprestimos[i]
+                .data_prevista
+            );
         }
     }
 
     if(!encontrou) {
 
-        printf("Nenhum emprestimo atrasado.\n");
+        printf(
+            "Nenhum emprestimo atrasado!\n"
+        );
     }
 }
