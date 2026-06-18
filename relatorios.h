ca1202f8848909b9170b87396bac8a4c2b38ab25
Eduardo Silva <166539950+Neterozk001@users.noreply.github.com>
2026-06-17 22:17:11 -0300

    Add includes and menu function prototype to relatorios.h
    Added includes for livros.h, emprestimos.h, and usuarios.h. Introduced menuRelatorios function prototype.

--- relatorios.h
+++ relatorios.h
@@ -1,5 +1,10 @@
 #ifndef RELATORIOS_H
 #define RELATORIOS_H
+#include "livros.h"
+#include "emprestimos.h"
+#include "usuarios.h"
+
+void menuRelatorios();
 
 void relatorio_AcervoDisponivel(
     Livro livros[],
